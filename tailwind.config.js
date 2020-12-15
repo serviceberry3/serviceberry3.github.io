8a24964e1b012d9251c65e04c3124829169d2f2a
Noah Weiner <noah.weiner@yale.edu>
2020-12-15 00:47:02 -0500

    install tailwind, mod buttons

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      backgroundColor: ['responsive', 'hover', 'focus', 'active'],
+    }
+  },
+  plugins: [],
+}
