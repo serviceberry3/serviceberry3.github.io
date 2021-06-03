3a395fad94cd8abaa4dabb9eb4ac2103d7656649
Noah Weiner <noah.weiner@yale.edu>
2021-06-03 14:45:26 -0400

    new blog post

---
+++ _posts/2021-06-03-heybitxhes.markdown
@@ -0,0 +1,8 @@
+---
+layout: post
+title: 'hey bitxhes '
+date: Thu Jun 03 00:35:33 EDT 2021
+categories: 
+blog: at
+---
+hey wassuon
---
+++ scripts/cpy_bins.sh
@@ -0,0 +1,5 @@
+#!/system/bin/sh
+
+BIN_PATH=/system/bin
+
+cp /data/data/com.termux/files/usr/bin/{ssh,ssh-add,ssh-keygen,vim,git} $BIN_PATH
